911ada2630624376b111d63a2f954e406c18b50f
heochaeyun <hur6149@gmail.com>
2025-06-17 08:37:35 +0900

    edit handler

--- db/merge_df.py
+++ db/merge_df.py
@@ -7,11 +7,16 @@ def load_df():
     # MongoDB
     mongo_df = load_mongo_data()
     mongo_df = mongo_preprocess(mongo_df)
+    print("laod_df내부에 있는 mongo_db", mongo_df.head())
+    print("load_df 내부에 있는 mongo_df columns:", mongo_df.columns.tolist())
 
     # PostgreSQL
     seed_df, user_df = load_postgres_data()
+    print("laod_df내부에 있는 seed_df", seed_df.head())
+    print("laod_df내부에 있는 user_df", user_df.head())
 
     merge = mongo_df.merge(seed_df, on="chapterId", how="inner")
+    print("laod_df내부에 있는 seed + mongo", merge.head())
     merged_df = merge.merge(user_df, on="userId", how="inner")
 
     return merged_df
--- db/postgres_handler.py
+++ db/postgres_handler.py
@@ -1,26 +1,28 @@
 import os
+from sqlalchemy import create_engine
 import pandas as pd
 import psycopg2
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv(override=True)
 
 def load_postgres_data():
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    host=os.getenv("DB_HOST")
+    port=os.getenv("DB_PORT")
+    dbname=os.getenv("DB_NAME")
+    user=os.getenv("DB_USER")
+    password=quote_plus(os.getenv("DB_PASSWORD"))
 
+    engine = create_engine(
+    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+    )
+    
     seed_query = "SELECT chapter_id, seed_money FROM invest_chapter ;"
     user_query = "SELECT user_id, sex, age FROM users;"
 
-    seed_df = pd.read_sql(seed_query, conn)
-    user_df = pd.read_sql(user_query, conn)
-
-    conn.close()
+    seed_df = pd.read_sql(seed_query, engine)
+    user_df = pd.read_sql(user_query, engine)
 
     seed_df.rename(columns={'chapter_id': 'chapterId'}, inplace=True)
     seed_df.rename(columns={'seed_money': 'seedMoney'}, inplace=True)
