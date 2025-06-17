c9e6538e0b6dcf4361519f81cccc9c773483a175
heochaeyun <hur6149@gmail.com>
2025-06-18 08:17:49 +0900

    add graph_filter & delete utils/preprocess.py

--- routers/graph.py
+++ routers/graph.py
@@ -5,60 +5,60 @@ router = APIRouter(prefix="/api/graph")
 
 @router.get("/graph1/all")
 def graph1(userId :str):
-    df = make_df_graph1(userId)
+    df = make_df_graph1(userId, filter=False)
     return df.to_dict(orient="records")
 
 @router.get("/graph1/week")
 def graph1(userId :str):
-    df = make_df_graph1(userId)
+    df = make_df_graph1(userId, filter=True)
     return df.to_dict(orient="records")
 
 @router.get("/graph2/1/all")
 def graph2_1(userId :str):
-    df = make_df_graph2_1(userId)
+    df = make_df_graph2_1(userId, filter=False)
     return df.to_dict(orient="records")
 
 @router.get("/graph2/1/week")
 def graph2_1(userId :str):
-    df = make_df_graph2_1(userId)
+    df = make_df_graph2_1(userId, filter=True)
     return df.to_dict(orient="records")
 
 @router.get("/graph2/2/all")
 def graph2_2(userId :str):
-    df = make_df_graph2_2(userId)
+    df = make_df_graph2_2(userId, filter=False)
     return df.to_dict(orient="records")
 
 @router.get("/graph2/2/week")
 def graph2_2(userId :str):
-    df = make_df_graph2_2(userId)
+    df = make_df_graph2_2(userId, filter=True)
     return df.to_dict(orient="records")
 
 @router.get("/graph2/3/all")
 def graph2_3(userId :str):
-    df = make_df_graph2_3(userId)
+    df = make_df_graph2_3(userId, filter=False)
     return df.to_dict(orient="records")
 
 @router.get("/graph2/3/week")
 def graph2_3(userId :str):
-    df = make_df_graph2_3(userId)
+    df = make_df_graph2_3(userId, filter=True)
     return df.to_dict(orient="records")
 
 @router.get("/graph3/all")
 def graph3(userId :str):
-    df = make_df_graph3(userId)
+    df = make_df_graph3(userId, filter=False)
     return df.to_dict(orient="records")
 
 @router.get("/graph3/week")
 def graph3(userId :str):
-    df = make_df_graph3(userId)
+    df = make_df_graph3(userId, filter=True)
     return df.to_dict(orient="records")
 
 @router.get("/graph4/all")
 def graph4(userId :str):
-    df = make_df_graph4(userId)
+    df = make_df_graph4(userId, filter=False)
     return df.to_dict(orient="records")
 
 @router.get("/graph4/week")
 def graph4(userId :str):
-    df = make_df_graph4(userId)
+    df = make_df_graph4(userId, filter=True)
     return df.to_dict(orient="records")
